eb6a7f198a0f480f2bbb61cd345daf34b53a979e
NikGerUni <nikger@students.softuni.bg>
2024-11-21 20:15:11 +0200

    Added 404.php

---
+++ wp-content/themes/my-theme/404.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Template Name: 404 Page
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="content">
+    <h1>Oops! Page Not Found</h1>
+    </div>
+
+    <?php get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
+
+<?php wp_footer(); ?>
