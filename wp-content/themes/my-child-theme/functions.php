70337c15d8d8a924bebfb214ec96d518af17c043
NikGerUni <nikger@students.softuni.bg>
2024-11-21 12:15:09 +0200

    Edited  My Child Theme functions.php in OOP style

--- wp-content/themes/my-child-theme/functions.php
+++ wp-content/themes/my-child-theme/functions.php
@@ -1,13 +1,21 @@
 <?php
-// Enqueue parent theme styles
-function my_child_theme_enqueue_styles() {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    
-    // Enqueue child theme styles
-    wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ), null );
+class MyChildTheme {
+    public function __construct() {
+        // Hook into WordPress to enqueue styles
+        add_action('wp_enqueue_scripts', [$this, 'my_child_theme_enqueue_styles']);
+    }
 
-    // Enqueue compiled-style.css if used in parent theme
-    wp_enqueue_style( 'compiled-style', get_template_directory_uri() . '/css/compiled-style.css', array( 'parent-style' ), null );
+    public function my_child_theme_enqueue_styles() {
+        // Enqueue parent theme styles
+        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 
+        // Enqueue child theme styles
+        wp_enqueue_style('child-style', get_stylesheet_uri(), ['parent-style'], null);
+
+        // Enqueue compiled-style.css if used in parent theme
+        wp_enqueue_style('compiled-style', get_template_directory_uri() . '/css/compiled-style.css', ['parent-style'], null);
+    }
 }
-add_action('wp_enqueue_scripts', 'my_child_theme_enqueue_styles');
+
+// Initialize the class
+new MyChildTheme();
